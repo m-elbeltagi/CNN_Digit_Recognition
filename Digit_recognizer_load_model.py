1425e7e0fbfa0497be8637fdddcdf79fcb15ea3f
Mohamed Elbeltagi <36570191+m-elbeltagi@users.noreply.github.com>
2023-02-21 21:43:39 -0500

    Update Digit_recognizer_load_model.py

--- Digit_recognizer_load_model.py
+++ Digit_recognizer_load_model.py
@@ -5,7 +5,7 @@ from Digit_Recognition_training_model import *
 
 
 
-image_test_flatenned = pd.read_csv(r'C:\Users\M\OneDrive - Carleton University\Documents\my_stuff\Projects\Kaggle_Digit_Recognizer\test.csv')
+image_test_flatenned = pd.read_csv(r'')
 
 ## reshape to be in format that model takes
 test_images_tensor = image_test_flatenned.to_numpy()
@@ -16,7 +16,7 @@ test_images_tensor = test_images_tensor.view(len(image_test_flatenned),1,28,28)
 
 ##  then later to load:
 loaded_model = DigitClassifier()
-loaded_model.load_state_dict(torch.load(r'C:\Users\M\OneDrive - Carleton University\Documents\my_stuff\Projects\Kaggle_Digit_Recognizer\DigitClassifier_saved_model_10epochs.pt'))
+loaded_model.load_state_dict(torch.load(r''))
 
 test_preds = loaded_model(test_images_tensor)
 
@@ -32,6 +32,6 @@ test_preds = test_preds.numpy()
 test_preds = pd.DataFrame(test_preds, index = [i+1 for i in np.arange(len(test_preds))], columns = ['Label'])
 test_preds.index.name = 'ImageId'
 
-# test_preds.to_csv(r'C:\Users\M\OneDrive - Carleton University\Documents\my_stuff\Projects\Kaggle_Digit_Recognizer\predicted_digits_submission_3.csv')
+# test_preds.to_csv(r'')
 
 print ('done')
